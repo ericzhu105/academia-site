780ca0203c40b6ae69d7287c5f2382f80899a9f7
Eric Zhu <ericzhu105@gmail.com>
2020-11-25 19:56:07 -0500

    commit

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'ericzhu105@gmail.com';
+  $receiving_email_address = 'contact@academiaedge.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
