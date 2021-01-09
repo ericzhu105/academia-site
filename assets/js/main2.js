71d9f35579e74b4ad13442248467a36c48075cac
Nitrostrider <maxwellzye@gmail.com>
2021-01-08 19:01:31 -0500

    split js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -9,7 +9,7 @@
     // Preloader
     $(window).on('load', function() {
       if ($('#preloader').length) {
-        $('#preloader').delay(100).fadeOut('slow', function() {
+        $('#preloader').delay(100).fadeOut('fast', function() {
           $(this).remove();
         });
       }
