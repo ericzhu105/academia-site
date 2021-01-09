0e302bf4e639c41031e7afd1ec4dc73cfff01092
Nitrostrider <maxwellzye@gmail.com>
2021-01-08 20:04:01 -0500

    update dropdown

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -9,7 +9,7 @@
     // Preloader
     $(window).on('load', function() {
       if ($('#preloader').length) {
-        $('#preloader').delay(100).fadeOut('fast', function() {
+        $('#preloader').delay(20).fadeOut('fast', function() {
           $(this).remove();
         });
       }
